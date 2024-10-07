e0acd02f8d81e5331df7eb5292f60b033f383d39
sasmithx <sasmithgit@gmail.com>
2024-10-07 10:13:36 +0530

    Add ItemDTO class with Lombok annotations and implement interfaces

---
+++ src/main/java/lk/ijse/springpos/dto/impl/ItemDTO.java
@@ -0,0 +1,20 @@
+package lk.ijse.springpos.dto.impl;
+
+import lk.ijse.springpos.customObj.ItemResponse;
+import lk.ijse.springpos.dto.SuperDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ItemDTO implements SuperDTO, ItemResponse {
+    private String code;
+    private String name;
+    private BigDecimal price;
+    private int qty;
+    private String id;
+}
